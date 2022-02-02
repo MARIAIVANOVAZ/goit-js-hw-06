4b8a35fb4ade2221ea4e0045083738ad50f5cc27
MARIA IVANOVA <mashasedaya199317@gmail.com>
2022-02-02 23:37:53 +0200

    add 10 task

--- js/task-03.js
+++ js/task-03.js
@@ -1,31 +1,32 @@
-"use strict"
+"use strict";
 
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const gallery = document.querySelector('.gallery');
-gallery.classList.add('gallery')
+const gallery = document.querySelector(".gallery");
+gallery.classList.add("gallery");
 
-const createGallery = images.map(image => 
- `<li><img src="${image.url}" alt="${image.alt}" class ="image"></li>`
-).join("");
+const createGallery = images
+  .map(
+    ({ url, alt }) => `<li><img src="${url}" alt="${alt}" class ="image"></li>`,
+  )
+  .join("");
 // gallery.setAttribute("style",  "display: flex; justify-content: space-between; list-style: none");
 gallery.insertAdjacentHTML("beforeend", createGallery);
 
 console.log(gallery);
-
 
 // const gallery = document.querySelector('.gallery');
 
@@ -37,14 +38,9 @@ console.log(gallery);
 //     imageEl.src = url;
 //     imageEl.alt = alt;
 
-
 //     itemEl.append(imageEl);
-    
 
 // return itemEl;
 // };
 // console.log(makeGallery)
 // // gallery.append(itemEl);
-
-
-
--- js/task-10.js
+++ js/task-10.js
@@ -1,24 +1,36 @@
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
+const inputEl = document.querySelector("input");
+const buttonElCreate = document.querySelector("button[data-create]");
+const buttonElDestroy = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+let inputValue = 0;
 
-// <div id="controls">
-//   <input type="number" min="1" max="100" step="1" />
-//   <button type="button" data-create>Create</button>
-//   <button type="button" data-destroy>Destroy</button>
-// </div>
-
-// <div id="boxes"></div>
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число. Функция создает столько <div>, сколько указано в amount и добавляет их в div#boxes.
-
-// Размеры самого первого <div> - 30px на 30px.
-// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+buttonElCreate.addEventListener("click", createBoxes);
+inputEl.addEventListener("input", onInputChange);
+buttonElDestroy.addEventListener("click", destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
+function onInputChange(event) {
+  inputValue = event.currentTarget.value;
+}
+
+function createBoxes(amount) {
+  const boxes = [];
+  let boxSize = 30;
+
+  for (let i = 0; i < inputValue; i += 1) {
+    const box = document.createElement("div");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    boxSize += 10;
+    boxes.push(box);
+  }
+  boxesEl.append(...boxes);
 }
